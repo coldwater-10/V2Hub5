28b3a9d78c57fdc17ab76c55f93279d55b343559
coldwater-10 <91764021+coldwater-10@users.noreply.github.com>
2024-07-12 06:07:46 +0330

    update

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$subscription_urls = [
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/vmess_iran.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/ss_iran.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/trojan_iran.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/vless_iran.txt",
+  "https://igdux.top/~sub-tnetplus",
+  "https://links.achaemenidempireofpersia.uk/cR3Q5X#IrancpinetSub1",
+  "https://sub1.achaemenidempireofpersia.uk/#IrancpinetSub2",
+  "https://raw.githubusercontent.com/jahangirGH/sublinks/main/Fragsubhiddify.txt#Irancpi_Hiddify_Fragment",
+  "https://raw.githubusercontent.com/Surfboardv2ray/Subs/main/Raw",
+  "https://raw.githubusercontent.com/Surfboardv2ray/Subs/main/Realm",
+];
